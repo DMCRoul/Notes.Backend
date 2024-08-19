d73beb2c41fa5f0058c2ce86d8389be87516e397
DmCD <qvid9q@gmail.com>
2024-08-19 09:51:55 +0200

    ASP NET Core Web API

--- Notes.WebApi/Program.cs
+++ Notes.WebApi/Program.cs
@@ -1,6 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Notes.Application;
+using Notes.Application.Common.Mappings;
+using Notes.Application.Interfaces;
+using Notes.Persistence;
+using System;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Access the configuration
+var configuration = builder.Configuration;
+
+// Add services to the container.
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(INotesDbContext).Assembly));
+});
+
+builder.Services.AddApplication();
+builder.Services.AddPersistence(configuration);
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<NotesDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception exception)
+    {
+        // Handle exception (log it, etc.)
+    }
+}
 
 app.Run();
